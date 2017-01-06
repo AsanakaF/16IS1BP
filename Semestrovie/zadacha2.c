7a4ddbe56c49c3dd5eb89eac7e732b51624327c0
AsanakaF <alenkafirsova@mail.ru>
2017-01-07 01:16:07 +0400

    min

---
+++ Semestrovie/zadacha2.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <stdio.h>
+
+double f(double x) {
+
+return 2*x*(x-1);
+}
+
+int main() {
+int n = 0;
+double a, b, c, v1, v2, x;
+printf("Введите интервал a ");
+scanf("%lf", &a);
+printf("Введите интервал b ");
+scanf("%lf", &b);
+printf("Введите точность ");
+scanf("%lf", &c);
+do {
+a = (a + b) / 2;
+v1 = x - c;
+v2 = x + c;
+if (v1 <= v2)
+b = x;
+else a = x;
+
+n = n + 1;
+
+} while (fabs(a - b) >= c);
+printf("Минимум функции = %lf\n", a);
+return 0;
+}
