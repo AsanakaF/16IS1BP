3b6942f04fe04a13edfc99dddb953300f86343e0
AsanakaF <alenkafirsova@mail.ru>
2017-01-09 18:47:10 +0400

    gray

---
+++ Semestrovie/image/gray image/gray.c
@@ -0,0 +1,37 @@
+#include "qdbmp.h" 
+#include <stdio.h> 
+
+int main(void) 
+{ 
+BMP* bmp; 
+UCHAR r, g, b; 
+UINT width, height; 
+UINT x, y; 
+Int gray;
+
+bmp = BMP_ReadFile( "lena.bmp" ); 
+BMP_CHECK_ERROR( stderr, -1 ); /* если произошла ошибка,выводится уведомление и выход */ 
+
+width = BMP_GetWidth( bmp ); 
+height = BMP_GetHeight( bmp ); 
+
+for ( x = 0 ; x < width ; ++x ) 
+{ 
+for ( y = 0 ; y < height ; ++y ) 
+{ 
+/* Получить значения RGB пикселей */ 
+BMP_GetPixelRGB( bmp, x, y, &r, &g, &b ); 
+
+/* Приобразовываем значение RGB */ 
+Gray = (r + g + b)/3;
+BMP_SetPixelRGB( bmp, x, y, 255 - gray, 255 - gray, 255 - gray ); 
+} 
+} 
+
+BMP_WriteFile( bmp, "output.bmp"); 
+BMP_CHECK_ERROR( stderr, -2 ); 
+
+BMP_Free( bmp ); 
+
+return 0; 
+}
