c7fa36d697719cb83479bc3542d2d85c02d0ad26
AsanakaF <alenkafirsova@mail.ru>
2019-06-02 21:34:07 +0300

    Add files via upload

---
+++ LabsJava/Laba4/SMA.java
@@ -0,0 +1,24 @@
+package Laba4;
+import java.util.ArrayList;
+
+public class SMA {
+
+    public static void main(String[] args) {}
+
+    public static ArrayList<Double> calculate(ArrayList<Double> data, int window){
+        ArrayList<Double> out = new ArrayList<Double>();
+
+        Double[] arr = data.toArray(new Double[data.size()]);
+
+        for (int i = 0; i < arr.length - window - 1; i++) {
+            double val = arr[i];
+
+            for (int j = i + 1; j < i + window; j++)
+                val += arr[j];
+            val /= window;
+
+            out.add(val);
+        }
+        return out;
+    }
+}
